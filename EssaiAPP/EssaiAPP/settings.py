2eaee9876ac2891536231771ae9bd84e19353748
goudemars <frgllon@outlook.fr>
2022-07-05 20:08:00 +0000

    Add postgresql

--- EssaiAPP/EssaiAPP/settings.py
+++ EssaiAPP/EssaiAPP/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'EssaiAPP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER':'davide',
+        'PASSWORD':'essai',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
